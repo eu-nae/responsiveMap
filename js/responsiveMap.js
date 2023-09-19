e4871b7b0597e2f1721a3f54bf0085feb6d00151
eu-nae <143784684+eu-nae@users.noreply.github.com>
2023-09-19 12:10:01 +0900

    Add files via upload

---
+++ js/responsiveMap.js
@@ -0,0 +1,20 @@
+var toggleBtn = document.querySelector(".navbar-toggleBtn")
+var menu = document.querySelector(".navbar-menu")
+var icons = document.querySelector(".navbar-icons")
+// 화살표함수 function(){} (=) () => 화살표함수 
+
+// toggle 버튼에 클릭 이벤트 추가
+toggleBtn.addEventListener("click", ()=>{
+    menu.classList.toggle("active");
+    icons.classList.toggle("active");
+});
+
+// classList - 클래스 목록 나타냄
+// .toggle 숨겨져 있으면 나타내고, 나타나있으면 숨겨줌
+
+
+
+
+
+
+
